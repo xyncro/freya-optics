2d52ad2e28d6b44a30ab120098e48c51f9e950fc
kolektiv <andrew@xyncro.com>
2016-07-06 00:32:32 +0100

    http patch

---
+++ src/Freya.Optics.Http.Patch/Response.fs
@@ -0,0 +1,24 @@
+﻿namespace Freya.Optics.Http.Patch
+
+open Aether.Operators
+open Freya.Core
+open Freya.Optics
+open Freya.Optics.Http
+open Freya.Types.Http.Patch
+
+(* Response *)
+
+[<RequireQualifiedAccess>]
+module Response =
+
+    (* Headers *)
+
+    [<RequireQualifiedAccess>]
+    module Headers =
+
+        let private value_ key (tryParse, format) =
+                Response.header_ key
+            >-> Option.mapEpimorphism (tryParse >> Option.ofChoice, format)
+
+        let acceptPatch_ =
+            value_ "Accept-Patch" (AcceptPatch.tryParse, AcceptPatch.format)

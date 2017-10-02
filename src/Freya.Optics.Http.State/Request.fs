74ffe3267a9de26671f9914f9e720c1130b342b3
Pete Johanson <peter@peterjohanson.com>
2017-10-02 01:10:30 -0400

    Initial work on optics support for Cookie/Set-Cookie headers.
    * Closes xyncro/freya#197

---
+++ src/Freya.Optics.Http.State/Request.fs
@@ -0,0 +1,35 @@
+﻿namespace Freya.Optics.Http.Cors
+
+open Aether.Operators
+open Freya.Core
+open Freya.Optics
+open Freya.Optics.Http
+open Freya.Types.Http.State
+
+// Request
+
+/// Optics for working with individual elements of the request as part of the
+/// Freya state, including access to typed data using the Freya Types libraries
+/// where implemented.
+
+[<RequireQualifiedAccess>]
+module Request =
+
+    /// Optics for request headers, usually given as lenses from
+    /// State -> 'a option, where 'a is a strongly typed representation of an
+    /// optional header. Headers may be added and removed by using the common
+    /// Freya optic functionality with Option values.
+
+    [<RequireQualifiedAccess>]
+    module Headers =
+
+        let private value_ key (tryParse, format) =
+                Request.header_ key
+            >-> Option.mapEpimorphism (tryParse >> Option.ofResult, format)
+
+        /// A lens from State -> Cookie option, accessing
+        /// the optional Cookie header.
+
+        let cookie_ =
+            value_ "Cookie" (Cookie.tryParse, Cookie.format)
+
---
+++ src/Freya.Optics.Http.State/Response.fs
@@ -0,0 +1,34 @@
+﻿namespace Freya.Optics.Http.Cors
+
+open Aether.Operators
+open Freya.Core
+open Freya.Optics
+open Freya.Optics.Http
+open Freya.Types.Http.State
+
+// Response
+
+/// Optics for working with individual elements of the response as part of the
+/// Freya state, including access to typed data using the Freya Types libraries
+/// where implemented.
+
+[<RequireQualifiedAccess>]
+module Response =
+
+    /// Optics for response headers, usually given as lenses from
+    /// State -> 'a option, where 'a is a strongly typed representation of an
+    /// optional header. Headers may be added and removed by using the common
+    /// Freya optic functionality with Option values.
+
+    [<RequireQualifiedAccess>]
+    module Headers =
+
+        let private value_ key (tryParse, format) =
+                Response.header_ key
+            >-> Option.mapEpimorphism (tryParse >> Option.ofResult, format)
+
+        /// A lens from State -> SetCookie option,
+        /// accessing the optional Set-Cookie header.
+
+        let setCookie_ =
+            value_ "Set-Cookie" (SetCookie.tryParse, SetCookie.format)
